49cb4237e2b3145029d844bbbf9de74a0ca142e5
Shashank Aggarwal <shashankaggarwal13@gmail.com>
2020-05-04 00:42:58 +0530

    dynamically adding ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Burger from '../../components/Burger/Burger';
 import BurgerControls from '../../components/Burger/BuildControls/BuildControls';
 import Aux from '../../hoc/Auxi';
 
+const INGREDIENT_PRICES = {           //Set global bcz it will not be changed
+    cheese:0.5, 
+    salad:0.4,
+    meat:1.3,
+    bacon:0.7
+}
+
 class BurgerBuilder extends Component{
 
     state={
@@ -11,14 +18,29 @@ class BurgerBuilder extends Component{
             salad:1,
             bacon:0,
             meat:1
-        }
+        },
+        totalPrice:4
     }
     
+    addIngredientHandler = (type)=>{
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type]=updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice:newPrice,ingredients:updatedIngredients});
+    }
+
     render(){
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BurgerControls />
+                <BurgerControls 
+                    ingredientAdded={this.addIngredientHandler}/>
             </Aux>
         );
     }
