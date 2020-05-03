a0c51950197f07e35c3e1839eb8ab0e07f05b7fd
Shashank Aggarwal <shashankaggarwal13@gmail.com>
2020-05-03 19:40:42 +0530

    Burger file made

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const Burger = (props)=>{
+    const transformedIngredients=Object.keys(props.ingredients)
+    .map((igKey)=>{
+        return [...Array(props.ingredients[igKey])].map( (_ , i) => {
+            return (<BurgerIngredients key={igKey + i} type={igKey}/>);
+        })
+    });
     
-}
+    return (
+        <div>
+            <BurgerIngredients type='bread-top'/>
+            {transformedIngredients}
+            <BurgerIngredients type='bread-bottom'/>
+        </div>
+        );
+    }
+    
+export default Burger;
