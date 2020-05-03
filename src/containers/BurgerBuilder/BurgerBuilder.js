c64f055bc7eeaf4b052db65e11e3eaaecd7b9d2d
Shashank Aggarwal <shashankaggarwal13@gmail.com>
2020-05-03 23:59:25 +0530

    Added build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,10 +6,10 @@ class BurgerBuilder extends Component{
 
     state={
         ingredients:{
-            cheese:0,
-            salad:0,
+            cheese:1,
+            salad:1,
             bacon:0,
-            meat:0
+            meat:1
         }
     }
     
