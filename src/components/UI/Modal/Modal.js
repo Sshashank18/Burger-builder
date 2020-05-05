4064bf15ef56b7d5e1269bd45dd1e314b032aba2
Shashank Aggarwal <shashankaggarwal13@gmail.com>
2020-05-05 23:25:59 +0530

    Modal turned to class component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxi';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform:props.show ? 'translateY(0)':'translateY(-100vh)',
-                opacity:props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component{
+    //Turned Modal.js from functional component to class component so that we can see if the component should be updated or not and only then render (not unnecessary rendering)
+    shouldComponentUpdate(nextProps,nextState){
+        return nextProps.show != this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate(){
+        console.log("[Modal.js] will update.");
+    }
+
+    render(){
+        return(
+            <Aux>
+            <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+            <div 
+                className={classes.Modal}
+                style={{
+                    transform:this.props.show ? 'translateY(0)':'translateY(-100vh)',
+                    opacity:this.props.show ? '1' : '0'
+                }}>
+                {this.props.children}
+            </div>
+        </Aux>);
+    }
+} 
+
+
+export default Modal;
