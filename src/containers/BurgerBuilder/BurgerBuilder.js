b3a66b93fb5f6e668d20421328383c8d5d9fec20
Shashank Aggarwal <shashankaggarwal13@gmail.com>
2020-05-07 17:12:52 +0530

    Handling errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {           //Set global bcz it will not be changed
     cheese:0.5, 
@@ -99,13 +100,13 @@ class BurgerBuilder extends Component{
             },
             delieveryMethod:'fastest'
         }
-        // axios.post('/orders.json',order)
-        //     .then(response=>{
-        //         this.setState({loading: false, purchasing:false});
-        //     })
-        //     .catch(error=>{
-        //         this.setState({loading: false, purchasing:false});
-        //     });
+        axios.post('/orders.json',order)
+            .then(response=>{
+                this.setState({loading: false, purchasing:false});
+            })
+            .catch(error=>{
+                this.setState({loading: false, purchasing:false});
+            });
     }
 
     render(){
@@ -146,4 +147,4 @@ class BurgerBuilder extends Component{
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder,axios);
