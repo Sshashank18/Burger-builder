b7969b2bf86a1caba707691f7691eb79742d385f
Shashank Aggarwal <shashankaggarwal13@gmail.com>
2020-05-19 17:33:34 +0530

    Small changees

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,10 +9,6 @@ class Modal extends Component{
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
-    componentWillUpdate(){
-        console.log("[Modal.js] will update.");
-    }
-
     render(){
         return(
             <Aux>
